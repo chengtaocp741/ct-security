5f87399ef6ff430422df0f0f80f9cf84c63d9a40
ct <792043917@qq.com>
2018-10-23 17:42:42 +0800

    核心框架：Spring Boot 2.0 安全框架：Apache Shiro 1.4 视图框架：Spring MVC 5.0 持久层框架：MyBatis 3.3 定时器：Quartz 2.3 数据库连接池：Druid 1.1 日志管理：SLF4J 1.7、Log4j 页面交互：Vue2.x
    软件需求
    
    JDK1.8
    MySQL5.5+
    Maven3.0+

---
+++ ct-api/src/main/java/ct/administration/controller/ApiRegisterController.java
@@ -0,0 +1,48 @@
+package ct.administration.controller;
+
+
+import ct.administration.entity.UserEntity;
+import ct.administration.service.UserService;
+import ct.administration.common.utils.R;
+import ct.administration.common.validator.ValidatorUtils;
+import ct.administration.form.RegisterForm;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * 注册接口
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-26 17:27
+ */
+@RestController
+@RequestMapping("/api")
+@Api(tags="注册接口")
+public class ApiRegisterController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("register")
+    @ApiOperation("注册")
+    public R register(@RequestBody RegisterForm form){
+        //表单校验
+        ValidatorUtils.validateEntity(form);
+
+        UserEntity user = new UserEntity();
+        user.setMobile(form.getMobile());
+        user.setUsername(form.getMobile());
+        user.setPassword(DigestUtils.sha256Hex(form.getPassword()));
+        user.setCreateTime(new Date());
+        userService.insert(user);
+
+        return R.ok();
+    }
+}
